1bbb62e682f6b5be5e202030ddf700fc470a1cc9
muralishub <muralispost@gmail.com>
2021-02-09 10:12:47 +0000

    DC-3155: WIP make sure endpoints have clientService

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,8 +23,8 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"   %% "bootstrap-frontend-play-27" % "3.4.0",
-    "uk.gov.hmrc"   %% "play-frontend-hmrc"         % "0.40.0-play-27",
+    "uk.gov.hmrc"   %% "bootstrap-frontend-play-27" % "4.0.0",
+    "uk.gov.hmrc"   %% "play-frontend-hmrc"         % "0.41.0-play-27",
     "uk.gov.hmrc"   %% "play-frontend-govuk"        % "0.60.0-play-27",
     "uk.gov.hmrc"   %% "play-language"              % "4.10.0-play-27",
     "com.iheart"    %% "play-swagger"               % "0.10.2",
@@ -32,7 +32,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-27" % "3.4.0" % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-27" % "4.0.0" % Test,
     "org.scalatest"          %% "scalatest"              % "3.0.0" % Test,
     "org.jsoup"              % "jsoup"                   % "1.13.1" % Test,
     "com.typesafe.play"      %% "play-test"              % current % Test,
